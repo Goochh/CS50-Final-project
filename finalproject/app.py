c8a369884be62654585dfd45c80956e24c250c72
Bart Spreij <bartspreij@gmail.com>
2023-01-19 18:17:04 +0100

    Created a basic HTML template with Bootstrap

--- finalproject/app.py
+++ finalproject/app.py
@@ -1,10 +1,27 @@
-import os, sys, time
+from flask import Flask, redirect, render_template, url_for, request
 
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
+app = Flask(__name__, static_folder='static')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/programs", methods=["GET"])
+def programs():
+    return render_template("programs.html")
+
+@app.route("/diet", methods=["GET"])
+def diet():
+    return render_template("diet.html")
 
 
+@app.route("/statistics", methods=["GET"])
+def statistics():
+    return render_template("statistics.html")
+
+@app.route("/timer", methods=["GET"])
+def timer():
+    return render_template("timer.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
