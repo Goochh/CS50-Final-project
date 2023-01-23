103aac158de8d771a623795ab3a004829b3a527c
Bart Spreij <bartspreij@gmail.com>
2023-01-23 18:51:01 +0100

    Created some static pages such as the home page, programs page, recipe page and statistics page

--- finalproject/static/js/main.js
+++ finalproject/static/js/main.js
@@ -1,6 +1,16 @@
-    import Timer from "./Timer.js";
 
+const searchInput = document.getElementById('search-input');
+const cards = document.getElementsByClassName('card');
 
-    new Timer(
-        document.querySelector(".timer")
-    );
+searchInput.addEventListener('input', (e) => {
+  const searchValue = e.target.value.toLowerCase();
+  
+  for (let card of cards) {
+    const cardTitle = card.getElementsByClassName('card-title')[0].textContent.toLowerCase();
+    if (cardTitle.includes(searchValue)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  }
+});
